3a8af6e3a87f042dbe63c6ac2379f8f2fd75bf3f
akshatXD-hash <akshatpurohit23dec@gmail.com>
2026-02-27 17:30:16 +0530

    some footer tweaks

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -32,12 +32,7 @@ function Footer() {
           </nav>
 
           {/* Action Button */}
-          <button className="bg-blue-600 text-white px-8 py-3 rounded-full font-bold hover:bg-blue-700 transition-all shadow-lg shadow-blue-100 flex items-center gap-2">
-            Get Started
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
-            </svg>
-          </button>
+         
         </div>
 
         {/* Bottom Bar */}
